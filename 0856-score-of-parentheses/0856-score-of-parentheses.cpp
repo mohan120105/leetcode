1ca3880120d88ab9093dbe1ae098f7b14df5faeb
mohan <149851811+mohan120105@users.noreply.github.com>
2025-07-08 20:59:14 +0530

    Time: 0 ms (100%), Space: 8 MB (86.66%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        
+        int score=0;
+        int depth=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                depth++;
+            }
+
+            else{
+                depth--;
+                if(s[i-1]=='('){
+                    score+=1<<depth;
+                }
+            }
+        }
+        return score;
+        
+    }
+};
