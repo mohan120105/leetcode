93abc1c542bbd8350798dd99007c83e03272b6a5
mohan <149851811+mohan120105@users.noreply.github.com>
2025-08-07 07:42:28 +0530

    Time: 632 ms (30.5%), Space: 0B (100%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.sql
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -1,4 +1,4 @@
-/* Write your T-SQL query statement below */
-SELECT tweet_id
+# Write your MySQL query statement below
+select tweet_id
 from tweets
-where len(content)>15
+where length(content)>15;
