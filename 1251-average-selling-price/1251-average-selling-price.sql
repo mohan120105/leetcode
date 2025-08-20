7cad55ea8ed0c44b1acfdf16d4cea0a115bb38a1
mohan <149851811+mohan120105@users.noreply.github.com>
2025-08-20 06:20:18 +0530

    Time: 302 ms (93.5%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,8 +1,8 @@
 /* Write your T-SQL query statement below */
-SELECT P.product_id,ROUND(
+SELECT P.product_id,ISNULL(ROUND(
     COALESCE(SUM(P.PRICE*U.UNITS*1.0),0)/
     NULLIF(SUM(U.UNITS),0),2
-) average_price
+),0) average_price
 FROM PRICES P
 LEFT JOIN UNITSSOLD U
 ON P.PRODUCT_ID=U.PRODUCT_ID
