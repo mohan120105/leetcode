1426f600efa7094d63085a2ba266b60b6d204885
ch mohan <149851811+mohan120105@users.noreply.github.com>
2025-12-27 08:40:34 +0530

    Time: 800 ms (91.99%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -1,5 +1,4 @@
-/* Write your T-SQL query statement below */
-SELECT activity_date day,COUNT(DISTINCT SESSION_ID) active_users
-FROM ACTIVITY
-GROUP BY ACTIVITY_DATE
-HAVING ACTIVITY_DATE BETWEEN '2019-06-27' AND '2019-07-27'
+select activity_date as day,count(distinct user_id) as active_users
+from activity
+group by activity_date
+having activity_date between '2019-06-28' and '2019-07-27'
